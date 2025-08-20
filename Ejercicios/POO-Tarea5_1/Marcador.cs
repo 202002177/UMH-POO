0efed2d8744aca222a37c2ebaff657c2616776c1
202002177 <99231868+202002177@users.noreply.github.com>
2025-08-19 22:59:17 -0600

    Update Marcador.cs

--- Ejercicios/POO-Tarea5_1/Marcador.cs
+++ Ejercicios/POO-Tarea5_1/Marcador.cs
@@ -1,1 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directorio.Clases
+{
+    internal class Marcador
+    {
+        private double costoHora {  get; set; } 
+
+        private double horasTrabajadas { get; set;} 
+        private double salario {  get; set; }
+
+        public Marcador ()
+        {
+            this.costoHora = 50;
+        }
+
+        public double getSalario (double p_horastrabajadas)
+        {
+            this.salario = p_horastrabajadas*this.costoHora;
+            return this.salario;
+        }
+    }
+}
 
