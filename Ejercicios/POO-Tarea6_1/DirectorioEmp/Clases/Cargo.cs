2dc4a9117988c37d68f21b55226872fab38ec7c3
202002177 <99231868+202002177@users.noreply.github.com>
2025-08-21 19:09:43 -0600

    Update Cargo.cs

--- Ejercicios/POO-Tarea6_1/DirectorioEmp/Clases/Cargo.cs
+++ Ejercicios/POO-Tarea6_1/DirectorioEmp/Clases/Cargo.cs
@@ -1,52 +1,33 @@
 using System;
-using System.Collections.Generic;
-using DirectorioEmp.Clases;
 
-namespace DirectorioEmp
+namespace DirectorioEmp.Clases
 {
-    internal class Program
+    public class Cargo
     {
-        static void Main(string[] args)
+        public int numeroCargo { get; set; }
+        public string titulo { get; set; }
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFinal { get; set; }
+        public bool empleadoActual { get; set; }
+        public string empresa { get; set; }
+        public decimal salario { get; set; }
+        public string detalles { get; set; }
+
+        public Cargo()
         {
-            // instancia de Persona
-            Persona miPersona = new Persona("0801200312345", "Josue ", "Barahona", new DateTime(2003, 5, 15), "22334115", "99887766", "Honduras", "Tegucigalpa", "Colonia Miraflores", "Ingeniero");
+            
+        }
 
-            // instancias de Grado y Cargo
-            Grado gradoUniversitario = new Grado(1, "Universitario", "Universidad Metropolitana", "Ingeniería en informatica", new DateTime(2020, 1, 1), new DateTime(2025, 12, 15), "Pregrado", DateTime.Now.AddYears(1), "Honduras");
-            Cargo cargoActual = new Cargo(1, "Desarrollador de Software", new DateTime(2023, 1, 10), DateTime.Now, true, "Diuns", 17000.00m, "Desarrollo de aplicaciones web y móviles.");
-
-            miPersona.Grados.Add(gradoUniversitario);
-            miPersona.Cargos.Add(cargoActual);
-
-            Console.WriteLine($"--- Datos de la Persona ---");
-            Console.WriteLine($"ID: {miPersona.numeroID}");
-            Console.WriteLine($"Nombre Completo: {miPersona.nombres} {miPersona.apellidos}");
-            Console.WriteLine($"Fecha de Nacimiento: {miPersona.fechaNacimiento.ToShortDateString()}");
-            Console.WriteLine($"Profesión: {miPersona.profesion}");
-            Console.WriteLine();
-
-            Console.WriteLine("--- Grados Académicos ---");
-            foreach (var grado in miPersona.Grados)
-            {
-                Console.WriteLine($"Título: {grado.nombreTitulo}");
-                Console.WriteLine($"Institución: {grado.institucion}");
-                Console.WriteLine($"Nivel: {grado.nivelEducativo}");
-                Console.WriteLine($"Fechas: {grado.fechaInicio.ToShortDateString()} - {grado.fechaFinal.ToShortDateString()}");
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("--- Historial de Cargos ---");
-            foreach (var cargo in miPersona.Cargos)
-            {
-                Console.WriteLine($"Título del Cargo: {cargo.titulo}");
-                Console.WriteLine($"Empresa: {cargo.empresa}");
-                Console.WriteLine($"Salario: {cargo.salario:C}");
-                Console.WriteLine($"Fechas: {cargo.fechaInicio.ToShortDateString()} - {cargo.fechaFinal.ToShortDateString()}");
-                Console.WriteLine($"Empleado Actual: {(cargo.empleadoActual ? "Sí" : "No")}");
-                Console.WriteLine();
-            }
-
-            Console.ReadLine();
+        public Cargo(int numeroCargo, string titulo, DateTime fechaInicio, DateTime fechaFinal, bool empleadoActual, string empresa, decimal salario, string detalles)
+        {
+            this.numeroCargo = numeroCargo;
+            this.titulo = titulo;
+            this.fechaInicio = fechaInicio;
+            this.fechaFinal = fechaFinal;
+            this.empleadoActual = empleadoActual;
+            this.empresa = empresa;
+            this.salario = salario;
+            this.detalles = detalles;
         }
     }
 }
